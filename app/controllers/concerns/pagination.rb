02ca7dc8fb6e7504b120ac2853a5ea26dce7b620
songgz <sgzhe@163.com>
2023-12-19 11:20:04 +0800

    modify quiz

--- app/controllers/concerns/pagination.rb
+++ app/controllers/concerns/pagination.rb
@@ -8,10 +8,10 @@ module Pagination
 
   def pagination(records)
     {
-      total_count: records.total_count,
-      page_size: records.limit_value,
-      page_count: records.total_pages,
-      page_no: records.current_page
+      total_count: records&.total_count || 0,
+      page_size: records&.limit_value || 0,
+      page_count: records&.total_pages || 0,
+      page_no: records&.current_page || 0
     }
   end
 
