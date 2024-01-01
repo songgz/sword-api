a5036102bf80347236eceb4bc5fd58f0462cf8f7
songgz <sgzhe@163.com>
2024-01-01 21:25:19 +0800

    modify dictionary statistics

--- app/models/error_word.rb
+++ app/models/error_word.rb
@@ -6,7 +6,7 @@ class ErrorWord
   field :learns, type: Integer, default: 0
   field :reviews, type: Integer, default: 0
 
-  belongs_to :unit
-  belongs_to :dictionary
+  belongs_to :unit, optional: true
+  belongs_to :dictionary, optional: true
   embedded_in :learned_book
 end
